9c0ab5b40f2746c17c3d5a33d043d64ea2c8f646
Ashu <ashenshi@gmail.com>
2020-03-16 17:41:23 +0100

    files created and styled

---
+++ portfolio.js
@@ -0,0 +1,23 @@
+
+
+const htmlBar = document.querySelector('.bar-html');
+const cssBar = document.querySelector('.bar-css');
+const jsBar = document.querySelector('.bar-javascript');
+const reactBar = document.querySelector('.bar-react');
+const nodeBar = document.querySelector('.bar-node');
+
+let myEffect = new TimelineLite();
+
+myEffect.fromTo(htmlBar, .75, {width:`calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeOut})
+.fromTo(cssBar, .75, {width:`calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
+.fromTo(jsBar, .75, {width:`calc(0% - 6px)`}, {width: `calc(80% - 6px)`, ease: Power4.easeOut})
+.fromTo(reactBar, .75, {width:`calc(0% - 6px)`}, {width: `calc(80% - 6px)`, ease: Power4.easeOut})
+.fromTo(nodeBar, .75, {width:`calc(0% - 6px)`}, {width: `calc(70% - 6px)`, ease: Power4.easeOut})
+
+const contoller = new ScrollMagic.Controller()
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.skills',
+    triggerHook: 0
+})
+.setTween(myEffect)
+.addTo(contoller) 
